f7b9b6cca5fa85bf66ded04c02ae40d015e762e5
Matt Ball <matt@matt-ball.co.uk>
2019-11-20 16:33:14 -0800

    reinstate commander

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const log = require('./log')
 
 try {
+  console.log(process.cwd())
   module.exports = require(path.resolve(process.cwd(), '.postman'))
 } catch (e) {
   log.error('No .postman.json file found! Run postman setup.')
