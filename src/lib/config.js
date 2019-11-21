3175bfcc249764dc851c6acba5d986279babb5d0
Matt Ball <matt@matt-ball.co.uk>
2019-11-20 16:51:03 -0800

    remove debug

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,8 +2,6 @@ const path = require('path')
 const log = require('./log')
 
 try {
-  console.log(process.cwd())
-  console.log(path.resolve(process.cwd(), '.postman.json'))
   module.exports = require(path.resolve(process.cwd(), '.postman.json'))
 } catch (e) {
   log.error('No .postman.json file found! Run postman setup.')
