d3af1318fc516cdc9e39e80716ab8be64ebc5e3d
Matt Ball <matt@matt-ball.co.uk>
2020-02-07 10:52:40 -0800

    remove logs

--- src/lib/create-choices.js
+++ src/lib/create-choices.js
@@ -1,7 +1,3 @@
 module.exports = function createChoices (arr) {
-  return arr.map(({ name, id, uid }) => {
-    console.log('UID: ', uid)
-    console.log('ID: ', id)
-    return { name, value: uid || id }
-  })
+  return arr.map(({ name, id, uid }) => { return { name, value: uid || id } })
 }
