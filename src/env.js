ffb123c3ac18235986acd59f78a55d8ea33a6127
Matt Ball <matt@matt-ball.co.uk>
2020-02-26 15:47:32 -0800

    Clean bootstrap/sync functionality (#13)

--- src/env.js
+++ src/env.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const axios = require('axios')
 const { prompt } = require('enquirer')
+const config = require('./lib/config')
 const createChoices = require('./lib/create-choices')
 const log = require('./lib/log')
 const { POSTMAN_API_BASE } = require('./lib/constants')
 
 module.exports = async function env () {
-  const config = require('./lib/config')
-  const { POSTMAN_API_KEY, POSTMAN_WORKSPACE_ID, POSTMAN_ENVIRONMENT_FILENAME } = config
+  const { POSTMAN_API_KEY, POSTMAN_WORKSPACE_ID, POSTMAN_ENVIRONMENT_FILENAME } = config.get()
   const apiKeyParam = `?apikey=${POSTMAN_API_KEY}`
   const environments = await axios.get(`${POSTMAN_API_BASE}/workspaces/${POSTMAN_WORKSPACE_ID}/${apiKeyParam}`)
   const environmentChoices = createChoices(environments.data.workspace.environments)
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,9 +1,26 @@
+const fs = require('fs')
 const path = require('path')
 const log = require('./log')
+const { CONFIG_FILENAME } = require('./constants')
 
-try {
-  module.exports = require(path.resolve(process.cwd(), '.postman.json'))
-} catch (e) {
-  log.error('No/invalid .postman.json file found! Run postman setup.')
-  process.exit(1)
+module.exports = {
+  get: () => {
+    try {
+      return require(path.resolve(process.cwd(), CONFIG_FILENAME))
+    } catch (e) {
+      log.error(`No/invalid ${CONFIG_FILENAME} file found! Run postman setup.`)
+      process.exit(1)
+    }
+  },
+  set: (config, options) => {
+    try {
+      fs.writeFileSync(CONFIG_FILENAME, JSON.stringify(config, null, 2))
+
+      if (options.log) {
+        log.success('Postman CLI config saved!')
+      }
+    } catch (e) {
+      log.error(`Failed to write ${CONFIG_FILENAME} config file!`)
+    }
+  }
 }
