bf36db1e79efdca74a2ea56efe2e6329df8321a1
Matt Ball <matt@matt-ball.co.uk>
2020-03-04 12:46:26 -0800

    Split bootstrap sync (#15)
    * Decouple bootstrap and sync
    
    * version bump
    
    * lint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const bootstrap = require('./src/bootstrap')
 const clone = require('./src/clone')
 const env = require('./src/env')
 const setup = require('./src/setup')
@@ -15,7 +16,7 @@ program
 program
   .command('bootstrap')
   .description('Generate local files from Postman Collection')
-  .action(sync)
+  .action(bootstrap)
 
 program
   .command('sync')
