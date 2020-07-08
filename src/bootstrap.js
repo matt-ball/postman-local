616daedc38073fb20c15aee4abb9f1d9cc6fe4bf
Matt Ball <matt@matt-ball.co.uk>
2020-07-08 13:48:09 +0100

    ironing out some kinks

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,27 +1,37 @@
 const fs = require('fs')
+const axios = require('axios')
 const config = require('./lib/config')
+const constants = require('./lib/constants')
+const file = require('./lib/file')
 const log = require('./lib/log')
 const recurseCollection = require('./lib/recurse-collection')
 let wroteFile
 
 module.exports = async function bootstrap () {
   wroteFile = false
-  const { POSTMAN_TEST_DIR } = config.get()
-  await recurseCollection(mapItemToFile)
+  const { POSTMAN_DIR, POSTMAN_API_KEY, POSTMAN_ENVIRONMENT_ID } = config.get()
+  const collection = await recurseCollection(mapItemToFile)
+  file.collection.write(collection)
+
+  if (POSTMAN_ENVIRONMENT_ID) {
+    const apiKeyParam = `?apikey=${POSTMAN_API_KEY}`
+    const environment = await axios.get(`${constants.POSTMAN_API_BASE}/environments/${POSTMAN_ENVIRONMENT_ID}/${apiKeyParam}`)
+    file.environment.write(environment.data.environment)
+  }
 
   if (wroteFile) {
-    log.success(`Files written to ${POSTMAN_TEST_DIR} directory!`)
+    log.success(`Files written to ${POSTMAN_DIR} directory!`)
   } else {
     log.info('Nothing to bootstrap. No requests found within collection.')
   }
 }
 
 function mapItemToFile (item, context = '', type) {
-  const { POSTMAN_TEST_DIR } = config.get()
+  const { POSTMAN_DIR } = config.get()
   const obj = item[type]
   const scriptObj = (item.event && item.event.find((el) => el.listen === type)) || (!item.event && item.length && item.find((el) => el.listen === type))
   const script = scriptObj && scriptObj.script.exec.join('\n')
-  const path = `${POSTMAN_TEST_DIR}/${context}/${item.name || ''}`
+  const path = `${POSTMAN_DIR}/${context}/${item.name || ''}`
   const fileExtension = script ? 'js' : 'json'
   const filePath = `${path}/${type}.${fileExtension}`
   const fileNotBootstrapped = !fs.existsSync(filePath)
--- src/update.js
+++ src/update.js
@@ -5,11 +5,18 @@ const log = require('./lib/log')
 const { POSTMAN_API_BASE } = require('./lib/constants')
 
 module.exports = function update () {
-  const { POSTMAN_API_KEY, POSTMAN_COLLECTION_ID } = config.get()
+  const { POSTMAN_API_KEY, POSTMAN_COLLECTION_ID, POSTMAN_ENVIRONMENT_ID } = config.get()
   const collection = file.collection.read()
+  const environment = file.environment.read()
   const apiKeyParam = `?apikey=${POSTMAN_API_KEY}`
-  const apiAddress = `${POSTMAN_API_BASE}/collections/${POSTMAN_COLLECTION_ID}/${apiKeyParam}`
+  const collectionAddress = `${POSTMAN_API_BASE}/collections/${POSTMAN_COLLECTION_ID}/${apiKeyParam}`
+  const environmentAddress = `${POSTMAN_API_BASE}/environments/${POSTMAN_ENVIRONMENT_ID}/${apiKeyParam}`
 
-  axios.put(apiAddress, { collection })
+  axios.put(collectionAddress, { collection })
+
+  if (environment) {
+    axios.put(environmentAddress, { environment })
+  }
+
   log.success('Postman updated!')
 }
